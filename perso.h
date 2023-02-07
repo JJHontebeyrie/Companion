8495a55a914bbb234b9c5b4220574bbf55c83a83
JJHontebeyrie <122276814+JJHontebeyrie@users.noreply.github.com>
2023-02-07 23:47:00 +0100

    Valeurs personnelles à saisir

--- perso.h
+++ perso.h
@@ -21,18 +21,31 @@ int rotation = 3;
 // Mettez ici celle que vous estimez pour votre installation
 int residuel = 10; // en Watt
 
+// Affichage en nombres entiers
+// (par etienneroussel)
+bool nbrentier = true;  // ou false pour chiffres avec virgule
+
 // Modifiez les lignes suivantes en fonction de votre équipement
 int puissance = 5000; // production max en watt
 int cumulus = 3000; // puissance cumulus en watt
 
 // Chauffage par radiateurs électriques, mettez false sinon
-bool chauffageE = true;
+bool chauffageElectr = true;
 
 // Affichage de température si vous avez installé une sonde sur le cumulus
 // Mettez alors à true et vérifiez dans la routine 'decrypte' la bonne sonde
 bool sonde = false;
 
 // Localisation de votre ville et décalage horaire pour lever/coucher soleil
-double latitude = 44.8378;
-double longitude = -0.594;
-int utc_offset = +1;
+String latitude =  "44.8378"; // 90.0000 to -90.0000 negative for Southern hemisphere
+String longitude = "-0.594"; // 180.000 to -180.000 negative for West
+#define TIME_OFFSET 1UL * 3600UL // UTC + 0 hour (2UL * 3600L pour UTC + 1, etc...)
+
+// Données pour openweathermap.org ( API key et ville)
+String api_key = "**************************";
+String units = "metric";  // ou "imperial"
+String language = "fr";
+
+//*****************************************************************************
+//*******************    Fin des données personnelles    **********************
+//*****************************************************************************
